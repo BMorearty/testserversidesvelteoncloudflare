58ec25d3c40da3df0960acf181b8bfac36b1ad04
Brian Morearty <brian@morearty.org>
2023-01-10 01:13:07 -0800

    dynamic

--- src/routes/serverside/+page.server.ts
+++ src/routes/serverside/+page.server.ts
@@ -1,5 +1,5 @@
 import type { PageServerLoad } from './$types';
-import * as env from '$env/static/private';
+import * as env from '$env/dynamic/private';
 
 export const load: PageServerLoad = () => {
   return { env: JSON.stringify(env, null, 2) };
